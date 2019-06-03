e6feca6b886c95e081e5a70e561ec349191b8423
benjazor <benjazor@gmail.com>
2019-06-03 14:46:43 +0200

    Smooth Movement + Rotation
    I made fucntions that makes the movements and rotations smoother using coroutines.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,8 +18,9 @@ public class Player : MonoBehaviour
 
     // Movement varibables
     float lastMove = 0; // Time of the last movement input
-    float movementDuration = 0.2f; // Movement duration in seconds
+    float movementDuration = 0.4f; // Movement duration in seconds
 
+    // Execute when a player is created
     private void Start()
     {
         // Create the triggers required for movement
@@ -31,52 +32,38 @@ public class Player : MonoBehaviour
         UpdateTriggers();
     }
 
-     // FixedUpdata is called once per frame
+     // FixedUpdate is called once per frame
     void FixedUpdate()
     {
-        MoveInput();
+        // Check for input when the player isn't moving
+        if (!Moving()) { MoveInput(); }
 
         // Debug
-        string debugString = "";
-
-        debugString += "DEBUG\n";
-        debugString += "Position: " + transform.position + "\n";
-        debugString += "Rotation: " + transform.rotation + "\n";
-
-        debugText.text = debugString;
+        debugText.text = "DEBUG";
     }
 
     // Boolean value telling if the player is moving or not
-    bool Moving() => (Time.time < (lastMove + 0.2));
-
+    bool Moving() => (Time.time < (lastMove + movementDuration));
 
     // Execute player movement if he is not moving.
     void MoveInput()
     {
-        if (!Moving())
+        if (Input.GetKey("z"))
         {
-            // Move Forwoard
-            if (Input.GetKey("z"))
-            {
-                Move(1);
-            }
-            // Rotate Left
-            else if (Input.GetKey("q") && !Input.GetKey("d"))
-            {
-                Move(2);
-            }
-            // Rotate Right
-            else if (Input.GetKey("d") && !Input.GetKey("q"))
-            {
-                Move(3);
-            }
-            // Jump
-            else if (Input.GetKey("space"))
-            {
-                Move(4);
-            }
+            Move(1); // Move Forwoard
         }
-        
+        else if (Input.GetKey("q") && !Input.GetKey("d"))
+        {
+            Move(2); // Rotate Left
+        }
+        else if (Input.GetKey("d") && !Input.GetKey("q"))
+        {
+            Move(3); // Rotate Right
+        }
+        else if (Input.GetKey("space"))
+        {
+            Move(4); // Jump
+        }
     }
 
     void Move(int direction)
@@ -84,11 +71,15 @@ public class Player : MonoBehaviour
         // Record the time of movement
         lastMove = Time.time;
 
-        // Move in the right direction
+        // Move in the coorect direction
+        //  0: Idle
+        //  1: Forward
+        //  2: Rotate Left
+        //  3: Rotate Right
+        //  4: Jump
         switch (direction)
         {
-            // Move Forward
-            case 1: 
+            case 1: // Move Forward
                 // Make a rotation when facing a wall
                 if (TriggerA.GetComponent<Trigger>().status)
                 {
@@ -97,7 +88,8 @@ public class Player : MonoBehaviour
                 // Simply move forward
                 else if (TriggerB.GetComponent<Trigger>().status)
                 {
-                    transform.Translate(new Vector3(0, 0, 1));
+                    //transform.Translate(new Vector3(0, 0, 1));
+                    StartCoroutine(SmoothMovement(new Vector3(0, 0, 1)));
                 }
                 // Move to another face of the block
                 else if (!(TriggerD.GetComponent<Trigger>().status || TriggerE.GetComponent<Trigger>().status))
@@ -106,13 +98,13 @@ public class Player : MonoBehaviour
                     transform.Rotate(new Vector3(90, 0, 0));
                 }
                 break;
-            // Rotate Left
-            case 2:
-                transform.Rotate(new Vector3(0, -90, 0));
+            case 2: // Rotate Left
+                //transform.Rotate(new Vector3(0, -90, 0));
+                StartCoroutine(SmoothRotation(new Vector3(0, -90, 0)));
                 break;
-            // Rotate Right
-            case 3: 
-                transform.Rotate(new Vector3(0, 90, 0));
+            case 3: // Rotate Right
+                //transform.Rotate(new Vector3(0, 90, 0));
+                StartCoroutine(SmoothRotation(new Vector3(0, 90, 0)));
                 break;
             // Jump
             case 4:
@@ -122,6 +114,26 @@ public class Player : MonoBehaviour
                     StartCoroutine(Jump(10));
                 }
                 break;
+        }
+        UpdateTriggers();
+    }
+
+    IEnumerator SmoothMovement(Vector3 vector)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            transform.Translate(vector / 10);
+            yield return new WaitForSeconds(movementDuration / 15);
+        }
+        UpdateTriggers();
+    }
+
+    IEnumerator SmoothRotation(Vector3 vector)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            transform.Rotate(vector / 10);
+            yield return new WaitForSeconds(movementDuration / 15);
         }
         UpdateTriggers();
     }
